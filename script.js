3b6a9f3674c3f3a3912037df76c99518a4af95f0
Fazliddin Asadov <asadovfazliddin11@gmail.com>
2022-08-06 16:08:19 +0300

    categories add

---
+++ script.js
@@ -0,0 +1,87 @@
+"use strict";
+// class Car {
+//   constructor(name, price, mph) {
+//     (this.name = name), (this.price = price), (this.mph = mph);
+//   }
+//   static register(){
+//     console.log("asdasd");
+//   }
+//   info() {
+//     console.log(`This is ${this.name}.Price ${this.price}.Speed ${this.mph}`);
+//   }
+// }
+// const bmw = new Car("bmw", "$55500", 350);
+// console.log(bmw);
+// bmw.info();
+// Car.register();
+
+const cardGroup = document.querySelector(".container");
+const clothesBox = document.querySelector(".boxes");
+const boxItem = document.querySelector("#clothes__boxes");
+const backBtn = document.querySelector(".back");
+
+const cardItems = [];
+let productsData = [];
+
+const kutibChiqar = async function () {
+  try {
+    const res = await fetch(" https://api.escuelajs.co/api/v1/categories");
+    const data = await res.json();
+    console.log(data);
+    productsData = data;
+
+    let html = "";
+    data.forEach((el) => {
+      html += `
+    <div class="card" id="">
+      <img src="${el.image}" alt="">
+      <h2>${el.name}</h2>
+    </div>
+`;
+    });
+    cardGroup.innerHTML = html;
+    const cardes = document.querySelectorAll(".card");
+    console.log(cardes);
+    cardes.forEach((card) => {
+      card.addEventListener("click", () => {
+        console.dir(card);
+        console.dir(card.attributes);
+        cardItems.push(card.attributes.id);
+        console.log(cardItems);
+        boxItem.classList.add("active");
+        clothes();
+      });
+    });
+  } catch (arr) {
+    console.log("error");
+  }
+};
+kutibChiqar();
+
+async function clothes() {
+  try {
+    const res = await fetch(
+      "https://api.escuelajs.co/api/v1/categories/2/products"
+    );
+    const cothesData = await res.json();
+    console.log(cothesData);
+    let html = "";
+    cothesData.forEach((item) => {
+      html += `
+        <div class="box">
+          <img src="${item.images}" alt="">
+          <h2>${item.title}</h2>
+        </div>
+    `;
+    });
+
+    clothesBox.innerHTML = html;
+  } catch (arr) {
+    console.log("error");
+  }
+}
+
+backBtn.addEventListener("click", () => {
+  boxItem.classList.remove("active");
+  clothes.remove();
+});
